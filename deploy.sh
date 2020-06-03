abfd2492af82c80d78412ae1a1797b526d0c4b29
Fabian-Robert Stöter <fabian-robert.stoter@inria.fr>
2020-06-03 15:24:31 +0200

    update manual build scripts

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,13 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'www.faroit.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:faroit/website.git master:gh-pages
+git push -f git@github.com:faroit/faroit.github.io.git master
 
 cd -
